18afcad8b3086e1d17186e9d7558aba2fbe004ac
castlele <castlelecs@gmail.com>
2025-03-01 19:26:15 +0300

    [feat]: add new love extensions

--- ext/loveext.lua
+++ ext/loveext.lua
@@ -26,3 +26,49 @@ function love.graphics.roundedRectangle(mode, x, y, w, h, rx, ry)
    end
    love.graphics.polygon(mode, pts)
 end
+
+---@param keyComb string
+---@return string[]
+function love.getKeys(keyComb)
+   return require("cluautils.string_utils").split(keyComb, "+")
+end
+
+---@param keys string[]
+function love.keyboard.isAllDown(keys)
+   if #keys == 0 then
+      return false
+   end
+
+   local isAllDown = true
+
+   for _, key in pairs(keys) do
+      isAllDown = isAllDown and love.keyboard.isDown(key)
+   end
+
+   return isAllDown
+end
+
+function love.quitIfNeeded(keymap)
+   local keys = love.getKeys(keymap)
+
+   if not love.keyboard.isAllDown(keys) then
+      return
+   end
+
+   love.event.quit(0)
+end
+
+function love.toggleRainbowBorders(config)
+   if not config.debug.isDebug then
+      return
+   end
+
+   local keys = love.getKeys(config.keymap.rainbowBorders)
+
+   if not love.keyboard.isAllDown(keys) then
+      return
+   end
+
+   config.debug.isRainbowBorders = not config.debug.isRainbowBorders
+end
+
